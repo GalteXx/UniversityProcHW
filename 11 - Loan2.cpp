69730dfc96527692b4c883a6184ed001d4c9807d
GalteXx <65533265+GalteXx@users.noreply.github.com>
2022-09-23 09:10:09 +0300

    Update 11 - Loan2.cpp

--- 11 - Loan2.cpp
+++ 11 - Loan2.cpp
@@ -7,7 +7,8 @@ int main()
     double S, m, n, p = 1;
     cout << "Enter S, m, n\n";
     cin >> S >> m >> n;
-    while ((S * (p / 100) * pow(1 + p / 100, n)) / (12 * (pow(1 + p / 100, n) - 1)) < m) p += 0.1;
+    while ((S * (p / 100) * pow(1 + p / 100, n)) / (12 * (pow(1 + p / 100, n) - 1)) < m) 
+        p += 0.1;
     cout << p;
     return 0;
 }
