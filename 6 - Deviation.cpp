f7c0515e2d2c60735e0d538cf1ec6262e63ebdf3
GalteXx <65533265+GalteXx@users.noreply.github.com>
2022-09-23 11:24:57 +0300

    Update 6 - Deviation.cpp

--- 6 - Deviation.cpp
+++ 6 - Deviation.cpp
@@ -9,12 +9,20 @@ int main(){
 	cout << "enter a\n";
 	cin >> a;
 	cout << "enter x\n";
+	if(a != int(a)) {
+		cout << "NO";
+		rerturn 0;
+	}
 	if(abs(x) < 1)
 		cout << a * log(x);
 	else {
+		
+		if(a - x*x < 0){
+			cout << "NO";
+			return 0;
+		}
+		
 		cout << sqrt(abs(a - x*x));
-		if(a - x*x < 0)
-			cout << 'i';
 
 	}
 		
