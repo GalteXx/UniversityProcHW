da9df4cc932a86e0c57385351ecdcb3038999aef
GalteXx <65533265+GalteXx@users.noreply.github.com>
2022-10-07 10:19:45 +0300

    Update 12 - Copy.cpp

--- 12 - Copy.cpp
+++ 12 - Copy.cpp
@@ -6,6 +6,10 @@ using namespace std;
 int main(){
     ifstream fl("file.txt");
     string a;
+    if(!fl.is_open()){
+        cout << "error";
+        return 0;
+    }
     while(getline(fl, a)){
         cout << a;
     }
